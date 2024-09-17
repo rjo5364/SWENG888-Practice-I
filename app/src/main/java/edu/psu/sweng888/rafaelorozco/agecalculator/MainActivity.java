4a68cb03bac5887ffac218610914b0b440342ddc
roroz <rjo5364@psu.edu>
2024-09-17 08:13:43 -0700

    Fixed some spacing in UI

--- app/src/main/java/edu/psu/sweng888/rafaelorozco/agecalculator/MainActivity.java
+++ app/src/main/java/edu/psu/sweng888/rafaelorozco/agecalculator/MainActivity.java
@@ -1,6 +1,11 @@
 package edu.psu.sweng888.rafaelorozco.agecalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+    // Declaring UI Elements
+    private EditText firstNameET;
+    private EditText lastNameET;
+    private DatePicker datePicker;
+    private Button calcAgeBtn;
+
 }
+
